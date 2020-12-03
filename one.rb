e8ec19d55cabdc0f18684f5d242dc98b55df3f5f
courey <coureyelliott@gmail.com>
2020-12-02 22:37:11 -0600

    improving first challenge by using classes

--- one.rb
+++ one.rb
@@ -1,45 +1,53 @@
-def comparison(static, var=[])
-    does_match = nil
+class ExpenseReport
 
-    var.each do |x|
-        if (static + x == 2020)
-            does_match = x
+    def initialize(expenses, depth)
+        @expenses = expenses
+        @depth = depth
+        @results = nil
+    end
+
+    def calculate_expenses
+        @expenses.each do |expense|
+            if @depth == 2
+                find_final_result(expense)
+            elsif @depth == 3
+                @expenses.each do |secondary_expense|
+                    sum_first_expenses = expense + secondary_expense
+                    multiplier = expense * secondary_expense
+                    find_final_result(sum_first_expenses, multiplier)
+                end
+            else
+                @results = "Depth unavailable."
+                break
+            end
+        end
+        puts @results
+    end
+
+    def find_final_result(expense, multiplier=expense)
+        diff = (2020 - expense)
+        if @expenses.include? diff
+            @results = diff * multiplier
         end
     end
 
-    return does_match
 end
 
-def find_two(values)
-    values.each do |x|
-        sum_comp = comparison(x, values)
-        if sum_comp
-            puts (x * sum_comp)
-            break
-        end
-    end
+
+def expenses
+    converted_expenses = []
+    f = File.readlines('./data_sets/one_data.rb')
+    f.each{ |line| converted_expenses << line.to_i}
+
+    return converted_expenses
 end
 
-def determine_answer(static, var=[])
-    var.each do |x|
-        if (var.include? (2020 - static - x))
-            third = (2020 - static - x)
-            puts static * x * third
-            break
-        end
-    end
-end
+expense_report = expenses
+challenge_1 = ExpenseReport.new(expense_report, 2)
+challenge_1.calculate_expenses
 
-def find_three(values)
-    values.each do |x|
-        determine_answer(x, values)
-    end
-end
+challenge_2 = ExpenseReport.new(expense_report, 3)
+challenge_2.calculate_expenses
 
-def check_input(values)
-    find_two(values)
-    find_three(values)
-end
-
-check_input([1826,1895,1427,1931,1651,1638,1507,1999,1886,1824,1902,1995,1945,1735,1823,1595,1936,1476,2010,1833,1932,1772,1791,1814,1783,1957,1901,1600,1502,1521,1812,1974,1880,1790,1672,1541,1807,426,1858,1699,1964,1996,1983,1498,1863,1976,1492,1930,1838,1941,1764,1929,1897,2009,1853,1753,1759,1860,1952,1988,1727,1751,1943,1830,1645,1907,1857,1714,1798,1944,1868,1630,959,2003,1987,1890,1962,1928,1872,1912,1709,1809,1650,1980,1737,1898,1817,1736,1991,1788,1776,1845,1854,1963,1554,1949,1576,1819,1960,699,1990,1757,1698,1596,304,1982,1477,1961,1636,1619,1946,1876,1592,1848,1707,1958,1874,1867,52,1881,1665,1463,1799,1979,677,1710,1869,1639,1787,1633,1956,1457,1581,2005,1782,1904,1910,62,1593,1695,1915,1922,1797,1715,1981,1925,1893,1562,1789,2008,1939,1669,1992,1652,117,1609,1686,1953,2007,599,1547,1959,1691,1520,1444,1641,887,1579,1778,1977,1768,1942,1713,1603,1926,1855,1655,1673,1887,1994,1839,1725,928,1771,1761,1971,1571,1806,1821,1624,1701,1436,1748,1921,1617,2004,1792,1732,1740,1831])
-
+challenge_unavailiable = ExpenseReport.new(expense_report, 4)
+challenge_unavailiable.calculate_expenses
